2f27fe25edb19263e3bff6b395b561fcf6e42cad
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-17 22:28:30 +0530

    Maximum Test Case Updated till UC1

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,1 +1,19 @@
+import  java.util.Scanner;
+public class MaximumTest {
+    public static Integer testMaximum(Integer x, Integer y, Integer z){
+        Integer max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        return max;
+    }
 
+    public static void main(String[] args) {
+        Integer xInt = 5, yInt = 4, zInt = 3;
+        Integer maximum = testMaximum(xInt, yInt, zInt);
+        System.out.println("The Largest of the Three Integers is = " +maximum);
+    }
+}
