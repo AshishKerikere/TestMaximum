0b9a2cc34fad29bd51e19ed10dfc1307e9083a9e
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-17 22:41:51 +0530

    Maximum Test Updated till UC5

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,18 +1,39 @@
-public class MaximumTest {
-    public static String testMaximum(String x, String y, String z){
-        String max = x;
-        if(y.compareTo(max) > 0){
+public class MaximumTest<T extends Comparable<T>> {
+    T x, y, z;
+
+    public MaximumTest(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public T maximum (){
+        return MaximumTest.maximum(x, y, z);
+    }
+
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0) {
             max = y;
         }
-        if(z.compareTo(max) > 0){
+        if(z.compareTo(max) > 0) {
             max = z;
         }
+        printMax(x, y, z, max);
         return max;
     }
 
+    public static <T> void printMax(T x, T y, T z, T max){
+        System.out.println("The Maximum one is = " +max);
+    }
+
     public static void main(String[] args) {
-        String xFloat = "Mumbai", yFloat = "Bengaluru", zFloat = "Chennai";
-        String maximum = testMaximum(xFloat, yFloat, zFloat);
-        System.out.println("The Largest of the Three Strings is = " +maximum);
+        Integer xInt = 5, yInt = 4, zInt = 3;
+        Float xFloat = 5.5f, yFloat = 4.4f, zFloat = 3.3f;
+        String xStr = "Mumbai", yStr = "Bengaluru", zStr = "Chennai";
+
+        new MaximumTest(xInt, yInt, zInt).maximum();
+        new MaximumTest(xFloat, yFloat, zFloat).maximum();
+        new MaximumTest(xStr, yStr, zStr).maximum();
     }
 }
